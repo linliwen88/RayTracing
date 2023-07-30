d46f2f4161402f9da685ea7b30d83b40d9e161ee
linliwen88 <s953329@gmail.com>
2023-07-29 17:09:33 -0700

    render sphere with normal as color

--- RayTraceShpere/shaders/object_fshader.glsl
+++ RayTraceShpere/shaders/object_fshader.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 fragPos;
+in vec3 fragPos; // world space
 
 uniform vec3 cameraPos;
 uniform vec3 sphereOrigin;
@@ -20,16 +20,29 @@ bool hit_sphere() {
 
 void main()
 {    
-    if(hit_sphere()) { // draw sphere
-        FragColor = vec4(0.83, 0.2, 0.2, 1.0);
+    vec3 rayDirection = fragPos - cameraPos;
+    vec3 oc = cameraPos - sphereOrigin;
+    float a = dot(rayDirection, rayDirection);
+    float b = 2.0 * dot(oc, rayDirection);
+    float c = dot(oc, oc) - sphereRadius*sphereRadius;
+    float discriminant = b*b - 4*a*c;
+
+    // Ray hit sphere, draw sphere
+    if(discriminant > 0.0) {
+        float t = (-b - sqrt(discriminant)) / (2 * a);
+        vec3 hitPoint = cameraPos + (rayDirection * t);
+        vec3 N = normalize(hitPoint - sphereOrigin);
+        N = 0.5 * (N + 1.0); // map normal to [0, 1]
+        FragColor = vec4(N, 1.0);
     }
-    else { // draw canvas
+    // Draw canvas
+    else {
         float windowHeight = 800.0;
 	    float t = gl_FragCoord.y / windowHeight;
 
 	    vec4 lightBlue = vec4(0.1, 0.1, 1.0, 1.0);
 	    vec4 white = vec4(1.0, 1.0, 1.0, 1.0);
 
-	    FragColor = (1.0 - t) * lightBlue + t * white;
+	    FragColor = (1.0 - t) * white + t * lightBlue;
     }
 }
